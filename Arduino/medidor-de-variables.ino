e1f40aa13f15b5f7615d4642c8d1db248053bf7a
Lprigara <lprigara@gmail.com>
2015-03-14 19:58:25 +0000

    Lector de medidas estable. Medidas recogidas: BPM, OxygenSaturation, AirFlow, Temperature.

---
+++ Arduino/medidor-de-variables.ino
@@ -0,0 +1,35 @@
+#include <eHealth.h>
+#include <PinChangeInt.h>
+int cont =0;
+int pin = 6;
+// The setup routine runs once when you press reset:
+void setup() {
+  Serial.begin(9600);  
+  eHealth.initPulsioximeter();
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  //attach interrupts to use the pulsioximeter.     
+  PCintPort::attachInterrupt(pin, readPulsioximeter, RISING);
+  delay(1000);
+  while (Serial.available()>0) {
+    Serial.print(eHealth.getBPM());  
+    Serial.print(" - ");  
+    Serial.print(eHealth.getOxygenSaturation());     
+    Serial.print(" - "); 
+    Serial.print(eHealth.getTemperature());
+    Serial.print(" - "); 
+    Serial.print(analogRead(A1));
+    Serial.print("\n");
+ }
+  
+}
+
+void readPulsioximeter() {  
+  cont ++;
+  if (cont == 50) { //Get only one of  50 measures to reduce the latency
+    eHealth.readPulsioximeter();  
+    cont = 0;
+  }
+}
